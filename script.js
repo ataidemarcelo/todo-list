a9ce0da303fd43f17d8c4562b7e9b5a6b76ac647
Marcelo Ataíde <msa.trybe@gmail.com>
2022-04-22 10:46:59 -0300

    Requisitos 5,6 - Ordene os itens da lista de tarefas por ordem de criação

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log('Project To Do List!!!');
+
+const btnAddTask = document.getElementById('criar-tarefa');
+const input = document.getElementById('texto-tarefa');
+const ol = document.getElementById('lista-tarefas');
+
+console.log(btnAddTask, input, ol);
+
+function addTask() {
+  btnAddTask.addEventListener('click', () => {
+    const text = input.value;
+    const li = document.createElement('li');
+    li.innerText = text;
+    ol.appendChild(li);
+    input.value = '';
+    // console.log('Add Task!: ', text);
+  });
+}
+
+addTask();
